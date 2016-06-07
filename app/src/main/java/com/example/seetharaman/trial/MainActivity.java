2adcebc728f22b3039669bc069411f60de4fa193
SathwikChadaga <chadagasathwik@gmail.com>
2016-06-07 20:55:54 +0530

    feat: added error messages in textInputLayout
    Yet to solve scrolling problem i.e. cannot scroll completely when keyboard pops up. Need help to solve this.

--- app/src/main/java/com/example/seetharaman/trial/MainActivity.java
+++ app/src/main/java/com/example/seetharaman/trial/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     EditText firstNameET, lastNameET, emailET, phoneET, collegeET;
-
+    TextInputLayout tilFirstName, tilLastName, tilEmail, tilPhone, tilSchoolName;
     //SUBMITTED INFORMATION IS STORED IN THE FOLLOWING VARIABLES
     String firstName, lastName, email, phone, college;
     int selectedItemPosition;
@@ -53,125 +54,89 @@ public class MainActivity extends AppCompatActivity {
         college = collegeET.getText().toString();
 
         Spinner spinner = (Spinner) findViewById(R.id.event_spinner);
+        assert spinner != null;
         selectedItemPosition = spinner.getSelectedItemPosition();
 
+        tilFirstName = (TextInputLayout) findViewById(R.id.til_first_name);
+        tilLastName = (TextInputLayout) findViewById(R.id.til_last_name);
+        tilEmail = (TextInputLayout) findViewById(R.id.til_email);
+        tilPhone = (TextInputLayout) findViewById(R.id.til_phone);
+        tilSchoolName = (TextInputLayout) findViewById(R.id.til_school_name);
+
         //Check whether the inputs given by user are valid:
-        int errorCode = checkValidity(firstName, lastName, email, phone, college);
-//      ERROR CODES RETURNED BY THE FUNCTION checkValidity:
-//        No error               :   errorcode produced is 0
-//        Error in First Name    :   errorcode produced is 1
-//        Error in Second Name   :   errorcode produced is 2
-//        Error in Email         :   errorcode produced is 3
-//        Error in Phone         :   errorcode produced is 4
-//        Error in schoolname    :   errorcode produced is 5
+        Boolean isValid = checkValidity();
 
-        //Show a Toast
-        //If inputs are not valid, ask to enter valid inputs
-        //If inputs are all valid, show successfully submitted
-        int duration = Toast.LENGTH_SHORT;
-        Context presentContext;
-        CharSequence text;
-        Toast submitInfoToast;
+        if(isValid) {
+            Toast submitToast = Toast.makeText(getApplicationContext(),  "Successfully submitted!", Toast.LENGTH_SHORT);
+            submitToast.show();
 
-        //Update the text to be displayed in Toast as per the errorcode produced by the function checkValidity
-        switch (errorCode) {
-
-            case 0: {
-                presentContext = getApplicationContext();
-                text = "Successfully submitted!";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-
-                Context context = view.getContext();
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case 1: {
-                presentContext = getApplicationContext();
-                text = "Enter valid First Name";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-                break;
-            }
-            case 2: {
-                presentContext = getApplicationContext();
-                text = "Enter valid Last Name";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-                break;
-            }
-            case 3: {
-                presentContext = getApplicationContext();
-                text = "Enter a valid Email ID";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-                break;
-            }
-            case 4: {
-                presentContext = getApplicationContext();
-                text = "Enter a valid Phone Number";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-                break;
-            }
-            case 5: {
-                presentContext = getApplicationContext();
-                text = "Enter valid School Name";
-                submitInfoToast = Toast.makeText(presentContext, text, duration);
-                submitInfoToast.show();
-                break;
-            }
+            Intent intent = new Intent(view.getContext(), MainActivity.class);
+            startActivity(intent);
         }
 
     }
 
-    public int checkValidity(String name1, String name2, String email, String phone, String schoolName) {
+    public Boolean checkValidity() {
 
-        int errorCode = 0;
-
-//      ERROR CODES RETURNED BY THE FUNCTION checkValidity:
-//        No error               :   errcode = 0
-//        Error in First Name    :   errcode = 1
-//        Error in Second Name   :   errcode = 2
-//        Error in Email         :   errcode = 3
-//        Error in Phone         :   errcode = 4
-//        Error in schoolname    :   errcode = 5
+        Boolean isValid = true;
 
         //Check validity of First Name
-        if (name1.length() == 0 || !name1.matches("[a-zA-Z ]+")) {
-            errorCode = 1;
-            return errorCode;
+        if (firstName.length() == 0) {
+            tilFirstName.setError("Field cannot be blank");
+            isValid = false;
+        }
+        if (!firstName.matches("[a-zA-Z ]+")) {
+            tilFirstName.setError("Enter valid First Name");
+            isValid = false;
+        } else {
+            tilFirstName.setErrorEnabled(false);
         }
 
         //Check validity of Last Name
-        if (name2.length() == 0 || !name2.matches("[a-zA-Z ]+")) {
-            errorCode = 2;
-            return errorCode;
+        if (lastName.length() == 0) {
+            tilLastName.setError("Field cannot be blank");
+            isValid = false;
+        }
+        if (!lastName.matches("[a-zA-Z ]+")) {
+            tilLastName.setError("Enter valid Last Name");
+            isValid = false;
+        } else {
+            tilLastName.setErrorEnabled(false);
         }
 
         //Check validity of Email Address
         Boolean emailBool = isValidEmail(email);
         if (!emailBool) {
-            errorCode = 3;
-            return errorCode;
+            tilEmail.setError("Enter a valid Email Address");
+            isValid = false;
+        }
+        else {
+            tilEmail.setErrorEnabled(false);
         }
 
         //Check validity of Phone Number
         Boolean phoneBool = isValidPhone(phone);
         if (!phoneBool) {
-            errorCode = 4;
-            return errorCode;
+            tilPhone.setError("Enter a valid Phone number");
+            isValid = false;
+        }
+        else {
+            tilEmail.setErrorEnabled(false);
         }
 
         //Check validity of School Name
-        if (schoolName.length() == 0 || !schoolName.matches("[a-zA-Z ]+")) {
-            errorCode = 5;
-            return errorCode;
+        if (college.length() == 0) {
+            tilSchoolName.setError("Field cannot be blank");
+            isValid = false;
         }
-
+        if (!college.matches("[a-zA-Z ]+")) {
+            tilSchoolName.setError("Enter valid School Name");
+            isValid = false;
+        } else {
+            tilSchoolName.setErrorEnabled(false);
+        }
         //If everything is valid return errorCode(which is 0, its initial value)
-        return errorCode;
+        return isValid;
     }
 
     public final static boolean isValidEmail(CharSequence target) {
