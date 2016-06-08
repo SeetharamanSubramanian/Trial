d2ee35b0f1cafe2e777afe873c886b6cb3197d48
Abhishek <abhishekaroor@gmail.com>
2016-06-08 11:19:31 +0530

    Added Toolbar and custom theme

--- app/src/main/java/com/example/seetharaman/trial/RegActivity.java
+++ app/src/main/java/com/example/seetharaman/trial/RegActivity.java
@@ -1,7 +1,9 @@
 package com.example.seetharaman.trial;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RegActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // Attaching the layout to the toolbar object
+        toolbar.setTitle("Registrations");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        setSupportActionBar(toolbar);
+
     }
 }
