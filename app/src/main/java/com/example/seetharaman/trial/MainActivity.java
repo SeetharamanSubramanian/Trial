0eafef3711a2e71cc009ac379641871cc5bbd4e3
keerthisuresh <keerthi1suresh@gmail.com>
2016-06-08 21:14:58 +0530

    Hide Keyboard on clicking outside it

--- app/src/main/java/com/example/seetharaman/trial/MainActivity.java
+++ app/src/main/java/com/example/seetharaman/trial/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.seetharaman.trial;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev){
+        View v = getCurrentFocus();
+
+        if (v!=null &&
+                (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE)&&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit")){
+            int scrcoords[] = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + v.getLeft()-scrcoords[0];
+            float y = ev.getRawY() + v.getTop()-scrcoords[1];
+
+            if (x<v.getLeft() || x>v.getRight() || y<v.getTop() || y>v.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public static void hideKeyboard(Activity activity)
+    {
+        if(activity!=null && activity.getWindow()!=null && activity.getWindow().getDecorView()!=null){
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(),0);
+        }
+    }
+
 }
