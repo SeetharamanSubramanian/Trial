c74cfd1d5616b65c9a79885eca01c9327de0a381
keerthisuresh <keerthi1suresh@gmail.com>
2016-06-11 01:50:56 +0530

    Validation
    Form validation for edittext fields and gender field made single clickable

--- app/src/main/java/com/example/seetharaman/trial/MainActivity.java
+++ app/src/main/java/com/example/seetharaman/trial/MainActivity.java
@@ -5,20 +5,36 @@ import android.content.Context;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etFirstName;
+    private EditText etLastName;
+    private EditText etInstitute;
+    private EditText etEmailAddrss;
+    private EditText etPhoneNumber;
+    private Button bRegister;
+    private RadioButton rbMale;
+    private RadioButton rbFemale;
     private Spinner sp_events;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         sp_events = (Spinner) findViewById(R.id.s_events);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -26,10 +42,46 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         assert sp_events != null;
         sp_events.setAdapter(adapter);
-
-
-
+        etFirstName = (EditText) findViewById(R.id.et_firstname);
+        etLastName = (EditText) findViewById(R.id.et_lastname);
+        etInstitute = (EditText) findViewById(R.id.et_institute);
+        etEmailAddrss = (EditText) findViewById(R.id.et_email);
+        etPhoneNumber = (EditText) findViewById(R.id.et_phonenumber);
+        rbFemale = (RadioButton) findViewById(R.id.rb_female);
+        rbMale = (RadioButton) findViewById(R.id.rb_male);
+        bRegister = (Button) findViewById(R.id.b_register);
     }
+
+    public void onClickRegisterBtn(View view) {
+        Validation.isName(etFirstName,true);
+        Validation.isName(etLastName,true);
+        Validation.hasText(etInstitute);
+        Validation.isPhoneNumber(etPhoneNumber, true);
+        Validation.isEmailAddress(etEmailAddrss, true);
+        if (checkValidation())
+            submitForm();
+        else
+            Toast.makeText(MainActivity.this, "Form contains error", Toast.LENGTH_LONG).show();
+    }
+
+    private void submitForm() {
+        // Submit your form here. your form is valid
+        Toast.makeText(this, "Submitting form...", Toast.LENGTH_LONG).show();
+    }
+
+    private boolean checkValidation() {
+        boolean ret = true;
+
+        if (!Validation.isName(etFirstName, true)) ret = false;
+        if (!Validation.isName(etLastName, true)) ret = false;
+        if (!Validation.hasText(etInstitute)) ret = false;
+        if (!Validation.isEmailAddress(etEmailAddrss, true)) ret = false;
+        if (!Validation.isPhoneNumber(etPhoneNumber, true)) ret = false;
+
+        return ret;
+    }
+
+
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev){
@@ -58,4 +110,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onClickRb(View view) {
+        if (view.getId()==R.id.rb_male && rbFemale.isChecked()){
+            rbFemale.setChecked(false);
+        }
+        if (view.getId()==R.id.rb_female && rbMale.isChecked()){
+            rbMale.setChecked(false);
+        }
+
+    }
 }
+
