9b2b0fa0cf22e11a85413d0da77f9609a1798c96
Sharath-girish <sharathgirish97@gmail.com>
2016-06-14 20:33:02 +0530

    Max length to contact number
    Added max length to contact no edittext field

--- app/src/main/java/com/example/seetharaman/trial/MainActivity.java
+++ app/src/main/java/com/example/seetharaman/trial/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
